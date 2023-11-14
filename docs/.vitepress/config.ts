eb062c97c18e2286b23bfd08b8dba9e104a07594
nicad <36125594+PBnicad@users.noreply.github.com>
2023-11-14 16:33:42 +0800

    init

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from "vitepress";
+
+// refer https://vitepress.dev/reference/site-config for details
+export default defineConfig({
+  lang: "zh-CN",
+  title: "STM32学习笔记",
+  description: "STM32学习笔记",
+  base: "/stm32notebook/",
+
+  themeConfig: {
+    nav: [
+      { text: "开始", link: "/stm32最小系统" },
+
+      // {
+      //   text: 'Dropdown Menu',
+      //   items: [
+      //     { text: 'Item A', link: '/item-1' },
+      //     { text: 'Item B', link: '/item-2' },
+      //     { text: 'Item C', link: '/item-3' },
+      //   ],
+      // },
+
+      // ...
+    ],
+
+    sidebar: [
+      {
+        // text: 'Guide',
+        items: [
+          { text: "STM32 最小系统构成", link: "/stm32最小系统" },
+          // ...
+        ],
+      },
+    ],
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,15 @@
+---
+layout: home
+
+hero:
+  name: STM32学习笔记
+  text: STM32F10
+  tagline: 使用的是普中科技玄武F103开发板
+  actions:
+    - theme: brand
+      text: 开始
+      link: /stm32最小系统
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/PBnicad/stm32notebook
+---
